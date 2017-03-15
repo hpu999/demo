6b5f07880d825d9af4a7f4a426a9e5701a21d6a2
王 胜 <wangsheng@cndoppler.cn>
2017-03-15 14:31:09 +0800

    增加struct fb_fix_screeninfo结构体

--- clear-screen.c
+++ clear-screen.c
@@ -7,23 +7,51 @@
 #include <sys/ioctl.h>
 #include <fcntl.h>
 
+/* 视频卡的各个属性
+ * struct fb_var_screeninfo {
+ *     __u32 xres;            // visable resolution in the X axis
+ *     __u32 yres;            // visable resolution in the Y axis
+ *     __u32 bits_per_pixel;  // number of bits required to hold a pixel
+ *     ... ...	
+ * }
+ */
+/* 视频硬件信息，用户无法改变
+ * struct fb_fix_screeninfo {
+ *     ... ...
+ *     unsigned long smem_start; // start of frame buffer memory
+ *     __u32 smem_len;           // length of frame buffer memory
+ *     ... ...
+ * }
+ */
+/* 规定了颜色映射
+ * struct fb_cmp {
+ *     ... ...
+ * }
+ */
 struct fb_var_screeninfo vinfo;
+struct fb_fix_screeninfo finfo;
 
 int main (int argc, char *argv[]) {
 
 	int fbfd, fbsize, i;
 	unsigned char *fbbuf;
+	int ret;
 	
 	/* open video memory */
-	if (open ("/dev/fb0", O_RDWR) < 0) {
+	if ((fbfd = open ("/dev/fb1", O_RDWR)) < 0) {
 		perror ("open");
 		exit (1);
 	}
 	printf ("open successed. \n");
 
+	if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo)){
+		printf("Error reading fixed information\n");
+		exit(2);
+	}
+ 
 	/* get variable display parameters */
 	if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo)) {
-		printf ("Bad vscreeninfo ioctl \n");
+		printf ("Bad vscreeninfo ioctl. ret = %d \n", ret);
 		exit (2);
 	}
 	printf ("ioctl successed. \n");
