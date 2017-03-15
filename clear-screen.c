194c21b9c3fb41932b05f40a24271062ae5520d3
王 胜 <wangsheng@cndoppler.cn>
2017-03-15 11:59:02 +0800

    修改clear-screen.c的打开文件为fb0

--- clear-screen.c
+++ clear-screen.c
@@ -13,21 +13,25 @@ int main (int argc, char *argv[]) {
 
 	int fbfd, fbsize, i;
 	unsigned char *fbbuf;
-
-	if (open ("/dev/fb2", O_RDWR) < 0) {
+	
+	/* open video memory */
+	if (open ("/dev/fb0", O_RDWR) < 0) {
 		perror ("open");
 		exit (1);
 	}
 	printf ("open successed. \n");
 
-	if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo) < 0) {
+	/* get variable display parameters */
+	if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo)) {
 		printf ("Bad vscreeninfo ioctl \n");
 		exit (2);
 	}
 	printf ("ioctl successed. \n");
 
+	/* size of frame buffer = (x-resolution * y-resoulation * bytes per pixel) */
 	fbsize = vinfo.xres * vinfo.yres * (vinfo.bits_per_pixel / 8);
 
+	/* map video memory */
 	if ((fbbuf = mmap (0, fbsize, PROT_READ | PROT_WRITE, MAP_SHARED, 
 							fbfd, 0)) == (void *)-1) {
 		perror ("mmap");
@@ -35,6 +39,7 @@ int main (int argc, char *argv[]) {
 	}
 	printf ("mmap successed. \n");
 
+	/* clear the screen */
 	for (i = 0; i < fbsize; ++i) {
 		*(fbbuf + i) = 0x0;
 	}
