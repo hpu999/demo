249a97cb1b313ea09d8826269eef9c4f0b5df4b3
王 胜 <wangsheng@cndoppler.cn>
2017-04-13 11:47:54 +0800

    mount.c完善挂载U盘，读取U盘，卸载U盘的小程序

--- mount.c
+++ mount.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int main (int argc, char *argv[]) {
+	int fd;
+
 	printf ("mount u pan \n");
 	system ("mkdir /mnt/usb");
 	system ("mount /dev/sda /mnt/usb");
-	system ("ls /mnt/usb");
+	system ("touch /mnt/usb/test");
+
+	fd = open ("/mnt/usb/test", O_RDWR);
+	if (fd < 0) {
+		perror ("open /mnt/usb/test");
+		system ("umount /mnt/usb");
+		system ("rmdir /mnt/usb");
+		return -1;
+	}
+	printf ("I can open the file in the usb. \n");
+	close (fd);
+
+	system ("rm /mnt/usb/test");
 	system ("umount /mnt/usb");
 	system ("rmdir /mnt/usb");
 	return 0;
