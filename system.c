a07b024d1a031971bce9b7cf75fbf392dabc2e0b
王 胜 <wangsheng@cndoppler.cn>
2017-04-13 11:58:04 +0800

    增加system函数执行的shell命令的退出状态

---
+++ system.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+int main()
+{
+    pid_t status;
+
+
+    status = system("ls");
+
+    if (-1 == status)
+    {
+        printf("system error!");
+    }
+    else
+    {
+        printf("exit status value = [0x%x]\n", status);
+
+        if (WIFEXITED(status))
+        {
+            if (0 == WEXITSTATUS(status))
+            {
+                printf("run shell script successfully.\n");
+            }
+            else
+            {
+                printf("run shell script fail, script exit code: %d\n", WEXITSTATUS(status));
+            }
+        }
+        else
+        {
+            printf("exit status = [%d]\n", WEXITSTATUS(status));
+        }
+    }
+
+    return 0;
+}
