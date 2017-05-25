b80a910d55ed1fce6573a2684e7a9025959f188f
王 胜 <wangsheng@cndoppler.cn>
2017-05-25 10:21:31 +0800

    在dopplerdisk.sh中增加对SD的分区的注释

--- dopplerdisk.sh
+++ dopplerdisk.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+# SD卡有多大容量
 sudo dd if=/dev/zero of=xrz.img bs=1M count=7580
 
-sudo sfdisk -D -H 255 -S 63 -C 966 xrz.img << EOF 
+# 开始对SD卡分区
+# 磁头255 扇区63 柱面966
+# 格式 <start>，<size>，<type [E,S,L,X,hex]>，<bootable [-,*]>
+# 第一个分区，从0开始 9个柱面 类型0x0C bootable
+# 第二个分区，从上次的哪个柱面开始 剩下所有的柱面(即容量) 其他的参数全部默认
+sudo sfdisk -D -H 255 -S 63 -C 966 xrz.img << EOF
 ,9,0x0C,*
 ,,,-
 EOF
