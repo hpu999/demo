35d9c9d637b141081bd9d54d35ed905d12b06b14
15903016222 <15903016222@users.noreply.github.com>
2017-12-27 21:10:24 +0800

    Update one_register_modu.c

--- one_register_modu.c
+++ one_register_modu.c
@@ -16,8 +16,10 @@
 #include <linux/io.h>
 #include <linux/delay.h>
 
+#define DEVICE_NAME "gpio_test"
+
 static int gMajor; /* major number of device */
-static struct class *dma_tm_class;
+static struct class *gpio_class;
 
 struct cmd_data {
 	unsigned int m_addr;
@@ -28,7 +30,7 @@ unsigned int *wbuf, *rbuf;
 
 struct cmd_data data;
 
-ssize_t register_read (struct file *filp, char __user * buf, size_t count,
+ssize_t gpio_read (struct file *filp, char __user * buf, size_t count,
 								loff_t * offset)
 {
 	copy_from_user (&data, buf, sizeof (struct cmd_data));
@@ -39,7 +41,7 @@ ssize_t register_read (struct file *filp, char __user * buf, size_t count,
 	return 0;
 }
 
-ssize_t register_write(struct file * filp, const char __user * buf, size_t count,
+ssize_t gpio_write(struct file * filp, const char __user * buf, size_t count,
 								loff_t * offset)
 {
 	copy_from_user (&data, buf, sizeof (struct cmd_data));
@@ -49,12 +51,12 @@ ssize_t register_write(struct file * filp, const char __user * buf, size_t count
 	return 0;
 }
 
-struct file_operations dma_fops = {
-	read:		register_read,
-	write:		register_write,
+struct file_operations gpio_fops = {
+	read:		gpio_read,
+	write:		gpio_write,
 };
 
-int __init register_init_module(void)
+int __init gpio_init_module(void)
 {
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,26))
 	struct device *temp_class;
@@ -64,55 +66,55 @@ int __init register_init_module(void)
 	int error;
 
 	/* register a character device */
-	error = register_chrdev(0, "register_test", &dma_fops);
+	error = register_chrdev(0, DEVICE_NAME, &gpio_fops);
 	if (error < 0) {
-		printk("register test driver can't get major number\n");
+		printk("gpio test driver can't get major number\n");
 		return error;
 	}
 	gMajor = error;
-	printk("register test major number = %d\n",gMajor);
+	printk("gpio test major number = %d\n",gMajor);
 
-	dma_tm_class = class_create(THIS_MODULE, "register_test");
-	if (IS_ERR(dma_tm_class)) {
+	gpio_class = class_create(THIS_MODULE, DEVICE_NAME);
+	if (IS_ERR(gpio_class)) {
 		printk(KERN_ERR "Error creating register test module class.\n");
-		unregister_chrdev(gMajor, "register_test");
-		return PTR_ERR(dma_tm_class);
+		unregister_chrdev(gMajor, DEVICE_NAME);
+		return PTR_ERR(gpio_class);
 	}
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,28))
-	temp_class = device_create(dma_tm_class, NULL,
-				   MKDEV(gMajor, 0), NULL, "register_test");
+	temp_class = device_create(gpio_class, NULL,
+				   MKDEV(gMajor, 0), NULL, DEVICE_NAME);
 #elif (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,26))
-	temp_class = device_create(dma_tm_class, NULL,
-				   MKDEV(gMajor, 0), "register_test");
+	temp_class = device_create(gpio_class, NULL,
+				   MKDEV(gMajor, 0), DEVICE_NAME);
 #else
-	temp_class = class_device_create(dma_tm_class, NULL,
+	temp_class = class_device_create(gpio_class, NULL,
 					     MKDEV(gMajor, 0), NULL,
-					     "register_test");
+					     DEVICE_NAME);
 #endif
 	if (IS_ERR(temp_class)) {
-		printk(KERN_ERR "Error creating register test class device.\n");
-		class_destroy(dma_tm_class);
-		unregister_chrdev(gMajor, "register_test");
+		printk(KERN_ERR "Error creating gpio test class device.\n");
+		class_destroy(gpio_class);
+		unregister_chrdev(gMajor, DEVICE_NAME);
 		return -1;
 	}
 
-	printk("register test Driver Module loaded\n");
+	printk("gpio test Driver Module loaded\n");
 	return 0;
 }
 
-static void register_cleanup_module(void)
+static void gpio_cleanup_module(void)
 {
-	unregister_chrdev(gMajor, "register_test");
-	device_destroy(dma_tm_class, MKDEV(gMajor, 0));
-	class_destroy(dma_tm_class);
+	unregister_chrdev(gMajor, DEVICE_NAME);
+	device_destroy(gpio_class, MKDEV(gMajor, 0));
+	class_destroy(gpio_class);
 
-	printk("register test Driver Module Unloaded\n");
+	printk("gpio test Driver Module Unloaded\n");
 }
 
 
-module_init(register_init_module);
-module_exit(register_cleanup_module);
+module_init(gpio_init_module);
+module_exit(gpio_cleanup_module);
 
-MODULE_DESCRIPTION("register test driver");
+MODULE_DESCRIPTION("gpio test driver");
 MODULE_LICENSE("GPL");
